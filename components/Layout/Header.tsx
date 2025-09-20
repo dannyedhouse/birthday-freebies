6c53cd549686cd28a3d3c8834b8e4e29d150df29
dannyedhouse <dannye7899@gmail.com>
2025-09-20 03:32:15 +0100

    upgrade major versions

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,13 +1,14 @@
 import {SuggestionModal} from '@/components/Modal/SuggestionModal'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const Header = () => {
   return (
     <div className="container mx-auto">
       <header className="flex justify-between items-center py-6 sm:py-10 gap-8 ">
-        <a href="/" className="flex">
+        <Link href="/" className="flex">
           <Image src="/logo.svg" width={400} height={400} alt="logo" className="bg-primary" />
-        </a>
+        </Link>
         <SuggestionModal />
       </header>
     </div>
